3de05c21f7d65979b2cf16e8dd731e0753b72c19
mo7md27 <mohammadabushera77@gmail.com>
2025-02-13 18:44:14 +0300

    best seller list view part one

--- lib/featuers/home/presentation/views/widgets/home_view_body.dart
+++ lib/featuers/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -18,9 +19,43 @@ class HomeViewBody extends StatelessWidget {
         FeaturedBooksListView(),
         SizedBox(height: 50),
         Text('Best Seller',
-        style: Styles.titleMedium,)
+        style: Styles.titleMedium,),
+
+        BestSellerListViewItem()
       
       
+      ],),
+    );
+  }
+}
+
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+      height: 125,
+      child: Row(children: [
+        AspectRatio(
+        aspectRatio: 2.5/4,
+        child: Container(
+          height: MediaQuery.of(context).size.height*.25,
+          width: MediaQuery.of(context).size.width*.3,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.white,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image:AssetImage(AssetsData.test))
+          ),
+        ),
+      ),
+        Column(children: [
+          
+      
+        ],)
       ],),
     );
   }
