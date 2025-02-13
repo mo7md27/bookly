7c064f15cd2d8f9259c5f127e68dd6be842e9466
mo7md27 <mohammadabushera77@gmail.com>
2025-02-13 18:04:40 +0300

    refactor feauterd book list

--- lib/featuers/home/presentation/views/widgets/home_view_body.dart
+++ lib/featuers/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
-import 'custom_list_view_item.dart';
+import 'featured_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -9,34 +9,11 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Column(children: [
-
       CustomAppBar(),
-      
-      FeaturedBooksListView()
+      FeaturedBooksListView(),
 
 
     ],);
   }
 }
-
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  SizedBox(
-      height: MediaQuery.of(context).size.height*.3,
-      child: ListView.builder(
-        
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.symmetric( horizontal:8),
-          child: CustomListViewItem(),
-        );
-      },),
-    );
-  }
-}
-
 
