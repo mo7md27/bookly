94900811350bd8547ff3feaed0db774bcd6618b7
mo7md27 <mohammadabushera77@gmail.com>
2025-02-13 17:59:29 +0300

    add featurd list view to home

--- lib/featuers/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/featuers/home/presentation/views/widgets/custom_app_bar.dart
@@ -9,7 +9,7 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24,vertical: 40),
+      padding: const EdgeInsets.only(left: 24,  right: 24,bottom: 10,    top: 40),
       child: Row(
         children: [
           Image.asset(AssetsData.logo,
--- lib/featuers/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/featuers/home/presentation/views/widgets/custom_list_view_item.dart
@@ -7,20 +7,17 @@ class CustomListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height*.23,
-      child: AspectRatio(
-        aspectRatio: 2.8/4,
-        child: Container(
-          height: MediaQuery.of(context).size.height*.25,
-          width: MediaQuery.of(context).size.width*.3,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            color: Colors.white,
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image:AssetImage(AssetsData.test))
-          ),
+    return AspectRatio(
+      aspectRatio: 2.8/4,
+      child: Container(
+        height: MediaQuery.of(context).size.height*.25,
+        width: MediaQuery.of(context).size.width*.3,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: Colors.white,
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image:AssetImage(AssetsData.test))
         ),
       ),
     );
