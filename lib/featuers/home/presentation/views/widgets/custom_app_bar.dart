1754eca9c1cb03843df8c2c1abc51cc80a9d8fea
mo7md27 <mohammadabushera77@gmail.com>
2025-02-20 16:37:17 +0300

    search view done

--- lib/featuers/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/featuers/home/presentation/views/widgets/custom_app_bar.dart
@@ -1,5 +1,7 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../../core/utils/assets.dart';
 
@@ -15,7 +17,11 @@ class CustomAppBar extends StatelessWidget {
           Image.asset(AssetsData.logo,
           height: 18,),
           Spacer(),
-          IconButton(onPressed: (){}, 
+          IconButton(onPressed: (){
+
+          GoRouter.of(context).push(AppRouter.kSearchView);
+
+          }, 
           icon: Icon(FontAwesomeIcons.magnifyingGlass,size: 24,))
         ],
       ),
