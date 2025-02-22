063ac6e73df1de9b434b67f4bbbfaef272a55acf
mo7md27 <mohammadabushera77@gmail.com>
2025-02-22 17:06:36 +0300

    create featured books methoads

---
+++ lib/featuers/home/presentation/manger/featured books cubit/featured_books_cubit.dart
@@ -0,0 +1,29 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/featuers/home/data/repos/home_repo.dart';
+import 'package:bookly/featuers/home/presentation/manger/featured%20books%20cubit/featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit <FeaturedBooksState>{
+
+FeaturedBooksCubit(this.homeRepo):super(FeaturedBooksInitial());
+
+
+final HomeRepo homeRepo;
+
+
+Future<void>fetchFeautredBooks()async{
+  emit(FeaturedBooksLoading());
+ var result= await homeRepo.fetchFeautredBooks();
+
+
+ result.fold(
+  (failure){
+    emit(FeaturedBooksFailure(failure.errMessage));
+  }, 
+  
+  (books){
+    emit(FeaturedBooksSuccess(books));
+  });
+}
+
+
+}
