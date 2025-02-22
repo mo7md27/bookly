2b4b3bca253c3664b1fec5becd0e84ca757aaad0
mo7md27 <mohammadabushera77@gmail.com>
2025-02-22 17:28:43 +0300

    newset books cubit done

---
+++ lib/featuers/home/presentation/manger/newset books cubit/newset_books_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/featuers/home/data/repos/home_repo.dart';
+import 'package:bookly/featuers/home/presentation/manger/newset%20books%20cubit/newset_books_state.dart';
+
+class NewsetBooksCubit extends Cubit<NewsetBooksState>{
+
+  NewsetBooksCubit(this.homeRepo):super(NewsetBooksInitial());
+
+  
+final HomeRepo homeRepo;
+
+
+Future<void>fetchNewsetBooksfetchFeautredBooks()async{
+  emit(NewsetBooksLoading());
+ var result= await homeRepo.fetchFeautredBooks();
+
+
+ result.fold(
+  (failure){
+    emit(NewsetBooksFailure(failure.errMessage));
+  }, 
+  
+  (books){
+    emit(NewsetBooksSuccess(books));
+  });
+}
+}
