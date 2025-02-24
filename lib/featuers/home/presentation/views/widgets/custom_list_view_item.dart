bb9c2227581b5e73f74a7ca5866c78679b4fb1a9
mo7md27 <mohammadabushera77@gmail.com>
2025-02-24 17:07:48 +0300

    used cached network image

--- lib/featuers/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/featuers/home/presentation/views/widgets/custom_list_view_item.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 
@@ -7,16 +8,19 @@ class CustomListViewItem extends StatelessWidget {
 final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        height: MediaQuery.of(context).size.height * .25,
-        width: MediaQuery.of(context).size.width * .3,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            image: DecorationImage(
-                fit: BoxFit.fill, image:
-                 NetworkImage(imageUrl))),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child:CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          placeholder: (context, url) => Center(
+            child: CircularProgressIndicator()),
+          errorWidget: (context, url, error) => Icon(Icons.error_outline_rounded),
+          
+          
+          ),
       ),
     );
   }
